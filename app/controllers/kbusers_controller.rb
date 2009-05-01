65a3649e090df68de48e3e4e86b806b557ef5f1f
jorahood <jorahood@5cd652fc-3c8e-420e-a729-68eecf2ccb59>
2009-05-01 13:41:17 +0000

    sort owned, authored, AND resourced docs on kbuser/show.dryml
    git-svn-id: svn+ssh://poblano.uits.indiana.edu/srv/svn/kb-support/trunk/worklists2@19225 5cd652fc-3c8e-420e-a729-68eecf2ccb59

--- app/controllers/kbusers_controller.rb
+++ app/controllers/kbusers_controller.rb
@@ -4,18 +4,15 @@ class KbusersController < ApplicationController
 
   auto_actions :all
 
-#  def index
-#    @kbusers_search_sort = Kbuser.apply_scopes(:search => [params[:search], :id],
-#      :order_by => parse_sort_param(:id,:lastname, :firstname, :email, :status))
-#  end
   def show
-    #decide whether the owned_docs or authored_docs collection is the main on based on doc count
+    #sort docs collections by size
     @kbuser = find_instance # Hobo needs @kbuser defined
     @owned = @kbuser.owned_docs
     @authored = @kbuser.authored_docs
-    @main_docs, @first_aside_docs = @authored.count > @owned.count ?
-      [@authored, @owned] : [@owned, @authored]
-    @paginated_docs = @main_docs.paginate(:page => params[:page])
+    @resourced = @kbuser.resourced_docs
+    @main_docs, @first_aside_docs, @second_aside_docs = 
+    [@kbuser.owned_docs, @authored, @resourced].sort_by {|docs| docs.length}.reverse
+    @paginated_docs = @main_docs.empty? ? @main_docs : @main_docs.paginate(:page => params[:page])
   end
 
 end
