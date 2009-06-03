55f958c054d8b75edd7859fa71de227c25e269fa
jorahood <jorahood@5cd652fc-3c8e-420e-a729-68eecf2ccb59>
2009-06-03 15:59:56 +0000

    * sketching search interface: specify the criteria for a search and show.dryml displays a table of matching docs. Hooray for automatic named_scopes and apply_scopes, which make the doc search so easy.
    git-svn-id: svn+ssh://poblano.uits.indiana.edu/srv/svn/kb-support/trunk/worklists2@19542 5cd652fc-3c8e-420e-a729-68eecf2ccb59

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -4,4 +4,17 @@ class SearchesController < ApplicationController
 
   auto_actions :all
 
+  def show
+    @search = find_instance
+    @result_docs =
+      Doc.apply_scopes(
+      :visibility_is => @search.visibility,
+      :volatility_is => @search.volatility,
+      :status_is => @search.status,
+      :author_is => @search.author,
+      :owner_is => @search.owner,
+      :importance_is => @search.importance,
+      :order_by => parse_sort_param(:doc)
+    )
+  end
 end
