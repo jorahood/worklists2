dc46c48f5d9ffdf061d56608fad1473a4d16c052
jorahood <jorahood@5cd652fc-3c8e-420e-a729-68eecf2ccb59>
2009-06-08 17:27:31 +0000

    * tweaks for vlad deployment.
    git-svn-id: svn+ssh://poblano.uits.indiana.edu/srv/svn/kb-support/trunk/worklists2@19554 5cd652fc-3c8e-420e-a729-68eecf2ccb59

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,4 +1,5 @@
 set :application, "worklists2"
 set :domain, "156.56.19.9"
-set :deploy_to, "/home/jorahood/vlad-test/#{application}"
+set :deploy_to, "/var/rails/#{application}"
 set :repository, 'svn+ssh://poblano.uits.indiana.edu/srv/svn/kb-support/branches/worklists2-r0.2-boilerlist.cgi'
+set :ssh_flags, '-A'
