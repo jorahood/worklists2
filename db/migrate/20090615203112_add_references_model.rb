d5d03fe76c8e846efe27f2212a73b81a2f0d7ee7
jorahood <jorahood@5cd652fc-3c8e-420e-a729-68eecf2ccb59>
2009-06-16 20:12:48 +0000

    * giving references composite primary keys fromid + toid with a monkeypatch to use Err's composite primary key migrations syntax. The schema dumper still doesn't know or care about the keys though. Need to work on that
    git-svn-id: svn+ssh://poblano.uits.indiana.edu/srv/svn/kb-support/trunk/worklists2@19682 5cd652fc-3c8e-420e-a729-68eecf2ccb59

--- db/migrate/20090615203112_add_references_model.rb
+++ db/migrate/20090615203112_add_references_model.rb
@@ -1,10 +1,9 @@
 class AddReferencesModel < ActiveRecord::Migration
   def self.up
-    create_table :references, :id => false do |t|
+    create_table :references, :primary_key => [:fromid, :toid] do |t|
       t.string :fromid
       t.string :toid
     end
-    execute("ALTER TABLE `references` ADD PRIMARY KEY (`fromid`, `toid`)")
   end
 
   def self.down
--- lib/monkey_patches/ar_base_extensions.rb
+++ lib/monkey_patches/ar_base_extensions.rb
@@ -1,10 +1,31 @@
-# add a class method to all ActiveRecord models that by default says they should not be imported from bell,
-# to be overridden in the models that do derive their data from bell tables
-
 module ActiveRecord
   class Base
+    # add a class method to all ActiveRecord models that by default says they should not be imported from bell,
+    # to be overridden in the models that do derive their data from bell tables
     def self.import_from_bell
       false
     end
   end
+
+  # support migrations creating tables with composite primary keys. Syntax is, e.g.,
+  # create_table :index_queue, :primary_key => [:content_id, :content_type] do |t|
+  # monkey patch overrides #to_sql in vendor/rails/activerecord/lib/active_record/connection_adapters/abstract/schema_definitions.rb
+  module ConnectionAdapters
+    class ColumnDefinition
+      def to_sql
+        if name.is_a? Array
+          column_sql = "PRIMARY KEY (#{name.join(',')})"
+        else
+          column_sql = "#{base.quote_column_name(name)} #{sql_type}"
+          column_options = {}
+          column_options[:null] = null unless null.nil?
+          column_options[:default] = default unless default.nil?
+          add_column_options!(column_sql, column_options) unless type.to_sym == :primary_key
+        end
+        column_sql
+      end
+      alias to_s :to_sql
+    end
+  end
+
 end
