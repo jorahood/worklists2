c51459b66233991a3d3c6995dcd63ebf36257d2b
jorahood <jorahood@5cd652fc-3c8e-420e-a729-68eecf2ccb59>
2009-06-25 18:54:49 +0000

    * hacking around incompatibility between Hobo and composite_primary_keys for Title model
    git-svn-id: svn+ssh://poblano.uits.indiana.edu/srv/svn/kb-support/trunk/worklists2@19745 5cd652fc-3c8e-420e-a729-68eecf2ccb59

--- app/models/title.rb
+++ app/models/title.rb
@@ -13,8 +13,15 @@ class Title < ActiveRecord::Base
     :foreign_key => 'docid'
   belongs_to :audience,
     :foreign_key => 'audience'
-#  set_primary_keys :docid, :audience
-  
+
+   # the fields declaration above interferes with composite primary keys'
+   # overriding the primary_key method to return the array set by
+   # set_primary_keys, setting it to 'id', which causes ar_extensions to fail to
+   # import titles. I'm using the fact that set_primary_key can also take
+   # a block as argument and will return the value of the block to hack
+   # around this incompatibility.
+  set_primary_key {primary_keys}
+  set_primary_keys :docid, :audience
 
   def self.import_from_bell
     true
