09ae3021c56701778ae81a845968c01303d8fe8e
jorahood <jorahood@5cd652fc-3c8e-420e-a729-68eecf2ccb59>
2009-08-04 18:22:10 +0000

    the revision name displayed on the front page was closely coupled to svn as scm, so I added a git method as well and moved the code out into some helper methods. The git method needs debugging.
    git-svn-id: svn+ssh://poblano.uits.indiana.edu/srv/svn/kb-support/trunk/worklists2@19929 5cd652fc-3c8e-420e-a729-68eecf2ccb59

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,16 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
+
+  def revision_name
+    svn_branch || git_branch || 'worklists2'
+  end
+
+  def svn_branch
+    /^URL: .*\/(\S+?\/\S+?)$/.match(`svn info`) ? $1 : nil
+  end
+
+  def git_branch
+#    /^\* (\S+?)$/.match(`git branch`) ? $1 : nil
+  nil
+  end
 end
