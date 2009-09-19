52226754f2002c67b89af888595708cd534535d6
jorahood <jorahood@5cd652fc-3c8e-420e-a729-68eecf2ccb59>
2009-09-19 20:02:10 +0000

    Set up to use Spork to run a spec server to speed up spec running
    git-svn-id: svn+ssh://poblano.uits.indiana.edu/srv/svn/kb-support/trunk/worklists2@20138 5cd652fc-3c8e-420e-a729-68eecf2ccb59

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,16 +1,22 @@
-# This file is copied to ~/spec when you run 'ruby script/generate rspec'
-# from the project root directory.
+require 'rubygems'
+require 'spork'
 ENV["RAILS_ENV"] ||= 'test'
-require File.dirname(__FILE__) + "/../config/environment" unless defined?(RAILS_ROOT)
+
+Spork.prefork do
+  # Loading more in this block will cause your tests to run faster. However, 
+  # if you change any configuration or code from libraries loaded here, you'll
+  # need to restart spork for it take effect.
+
+  # This file is copied to ~/spec when you run 'ruby script/generate rspec'
+  # from the project root directory.
+
+require File.dirname(__FILE__) + "/../config/environment"# unless defined?(RAILS_ROOT) (http://groups.google.com/group/sporkgem/browse_thread/thread/e107c82b2b63608a)
 require 'spec/autorun'
 require 'spec/rails'
 require 'spec/custom_matchers'
 
-# Requires supporting files with custom matchers and macros, etc,
-# in ./support/ and its subdirectories.
-Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each {|f| require f}
 
-Spec::Runner.configure do |config|
+  Spec::Runner.configure do |config|
   # If you're not using ActiveRecord you should remove these
   # lines, delete config/database.yml and disable :active_record
   # in your config/boot.rb
@@ -50,4 +56,33 @@ Spec::Runner.configure do |config|
   # == Notes
   #
   # For more information take a look at Spec::Runner::Configuration and Spec::Runner
+  end
+
+Spork.each_run do
+  # This code will be run each time you run your specs.
+  
+end
+
+# --- Instructions ---
+# - Sort through your spec_helper file. Place as much environment loading 
+#   code that you don't normally modify during development in the 
+#   Spork.prefork block.
+# - Place the rest under Spork.each_run block
+# - Any code that is left outside of the blocks will be ran during preforking
+#   and during each_run!
+# - These instructions should self-destruct in 10 seconds.  If they don't,
+#   feel free to delete them.
+#
+
+
+
+
+
+
+# Requires supporting files with custom matchers and macros, etc,
+# in ./support/ and its subdirectories.
+Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each {|f| require f}
+
+
+
 end
