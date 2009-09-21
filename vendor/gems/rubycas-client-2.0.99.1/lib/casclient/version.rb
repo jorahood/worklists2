cdfe1628fd40db9a5785d6ec7dd97bf40c39397e
jorahood <jorahood@5cd652fc-3c8e-420e-a729-68eecf2ccb59>
2009-09-21 00:45:14 +0000

    restoring rubycas-client gem files that got blown away by accident
    git-svn-id: svn+ssh://poblano.uits.indiana.edu/srv/svn/kb-support/trunk/worklists2@20163 5cd652fc-3c8e-420e-a729-68eecf2ccb59

--- vendor/gems/rubycas-client-2.0.99.1/lib/casclient/version.rb
+++ vendor/gems/rubycas-client-2.0.99.1/lib/casclient/version.rb
@@ -1,8 +1,8 @@
 module CASClient #:nodoc:
   module VERSION #:nodoc:
     MAJOR = 2
-    MINOR = 3
-    TINY  = 4
+    MINOR = 0
+    TINY  = 99
 
     STRING = [MAJOR, MINOR, TINY].join('.')
   end
