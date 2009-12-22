c86e7245830ea3ec5b7ef47b2134ce897d5f0ea0
jorahood <jorahood@5cd652fc-3c8e-420e-a729-68eecf2ccb59>
2009-12-22 21:38:49 +0000

    removed funky within_xpath scope and simplified the feature to not care where it finds the strings. All features passing except viewing boiler which needs to use Rack and the real routing system because ActionController::Integration::Session aint working
    git-svn-id: svn+ssh://poblano.uits.indiana.edu/srv/svn/kb-support/trunk/worklists2@20741 5cd652fc-3c8e-420e-a729-68eecf2ccb59

--- features/support/local_env.rb
+++ features/support/local_env.rb
@@ -4,37 +4,3 @@ require "spec/mocks"
 # support factory_girl
 require 'factory_girl'
 require 'spec/factories'
-# pretty formatting
-require File.expand_path(File.dirname(__FILE__) + '/textmate_formatter')
-
-# patch Webrat to allow Xpath selectors for "within" scoping
-
-# https://webrat.lighthouseapp.com/projects/10503/tickets/153-within-should-support-xpath
-# Webrat does not accept xpath selectors. This is a quick hack to make
-# it work for me. It tries to call css_at with the selector, if it
-# raises a syntax error the selector is passed to xpath_at. If that
-# fails then the original exception is raised.
-
-module Webrat
-  class Scope
-    protected
-
-    def xpath_scoped_dom
-      Webrat::XML.xpath_at(@scope.dom, @selector)
-    end
-
-    def scoped_dom
-      begin
-        Webrat::XML.css_at(@scope.dom, @selector)
-      rescue Nokogiri::CSS::SyntaxError, Nokogiri::XML::XPath::SyntaxError => e
-        begin
-          # That was not a css selector, mayby it's an xpath selector?
-          xpath_scoped_dom
-        rescue
-          # Raise original css syntax error if selector is not xpath either
-          raise e
-        end
-      end
-    end
-  end
-end
