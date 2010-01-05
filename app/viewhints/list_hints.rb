142b1cd16cd5a29cceb3a7f8c42ca87511fd57fa
jorahood <jorahood@5cd652fc-3c8e-420e-a729-68eecf2ccb59>
2010-01-05 20:40:19 +0000

    Hobo 1.0 requires using the children method in a view hint to define the main collection for a model, rather than inferring it from the use of :dependent => :destroy option on the association as before. See http://github.com/tablatom/hobo/commit/fa43aca55053d40960fcb3bf0a8ab8adcfee23ba
    git-svn-id: svn+ssh://poblano.uits.indiana.edu/srv/svn/kb-support/trunk/worklists2@20817 5cd652fc-3c8e-420e-a729-68eecf2ccb59

--- app/viewhints/list_hints.rb
+++ app/viewhints/list_hints.rb
@@ -1,4 +1,5 @@
 class ListHints < Hobo::ViewHints
+  children :listed_docs
   field_names :show_boilers => "Show Boiler Name",
     :wl1_import => "Import v1 Worklist",
     :wl1_clone => "Clone v1 Worklist"
