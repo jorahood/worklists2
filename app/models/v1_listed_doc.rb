089c5bdc0fb22c0e7767f647c9aab8ee2a9f934b
jorahood <jorahood@5cd652fc-3c8e-420e-a729-68eecf2ccb59>
2010-01-11 18:05:28 +0000

    v1_listed_doc model similar to v1_list. Indexed at /v1_listed_docs v1_listed_doc uses a composite primary key and belongs to lists
    git-svn-id: svn+ssh://poblano.uits.indiana.edu/srv/svn/kb-support/trunk/worklists2@20852 5cd652fc-3c8e-420e-a729-68eecf2ccb59

---
+++ app/models/v1_listed_doc.rb
@@ -0,0 +1,38 @@
+class V1ListedDoc < Worklists1
+
+  hobo_model # Don't put anything above this
+
+  fields do
+    searchdate   :datetime
+    modifieddate :datetime
+    category     :string
+    notes        :text
+    editornotes  :text
+    done         :boolean
+  end
+
+  set_table_name :docs_in_lists
+  set_primary_keys :listKey, :docKey
+  
+  belongs_to :v1_list,
+    :foreign_key => :listKey
+
+  # --- Permissions --- #
+
+  def create_permitted?
+    acting_user.administrator?
+  end
+
+  def update_permitted?
+    acting_user.administrator?
+  end
+
+  def destroy_permitted?
+    acting_user.administrator?
+  end
+
+  def view_permitted?(field)
+    true
+  end
+
+end
