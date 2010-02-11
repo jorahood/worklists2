b756850eb1379aeae6b32a0254727cd4f35935a5
jorahood <jorahood@5cd652fc-3c8e-420e-a729-68eecf2ccb59>
2010-02-11 18:59:50 +0000

    updated composite-primary-keys gem, ignoring rubymine project file for now, closing a tag I forgot about in application.dryml
    git-svn-id: svn+ssh://poblano.uits.indiana.edu/srv/svn/kb-support/trunk/worklists2@21032 5cd652fc-3c8e-420e-a729-68eecf2ccb59

--- config/environment.rb
+++ config/environment.rb
@@ -40,7 +40,7 @@ Rails::Initializer.run do |config|
     :lib => 'active_record/connection_adapters/oracle_adapter',
     :source => 'http://gems.rubyonrails.org'
   config.gem 'composite_primary_keys',
-    :version => '2.3.2'
+    :version => '2.3.5'
   #rubycas-client 2.0.99.1 makes the rubycas client compatible with IU CAS. I will keep a rebased version available under the iu tag at 
   #http://github.com/jorahood/rubycas-client/
   config.gem 'rubycas-client',
