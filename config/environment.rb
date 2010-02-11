74a95766514079a5b4ec10d62e18187e5bf61a43
jorahood <jorahood@5cd652fc-3c8e-420e-a729-68eecf2ccb59>
2010-02-11 18:59:55 +0000

    updating hobo to 0.9.106, etc
    git-svn-id: svn+ssh://poblano.uits.indiana.edu/srv/svn/kb-support/trunk/worklists2@21034 5cd652fc-3c8e-420e-a729-68eecf2ccb59

--- config/environment.rb
+++ config/environment.rb
@@ -11,8 +11,6 @@ RAILS_GEM_VERSION = '2.3.5' unless defined? RAILS_GEM_VERSION
 require File.join(File.dirname(__FILE__), 'boot')
 
 Rails::Initializer.run do |config|
-  config.gem 'hobo'
-
   # Settings in config/environments/* take precedence over those specified here.
   # Application configuration should go into files in config/initializers
   # -- all .rb files in that directory are automatically loaded.
@@ -45,12 +43,8 @@ Rails::Initializer.run do |config|
   #http://github.com/jorahood/rubycas-client/
   config.gem 'rubycas-client',
     :version => '2.0.99.1'
-  config.gem 'mislav-will_paginate',
-    :version => '~> 2.3.11',
-    :lib => 'will_paginate',
-    :source => 'http://gems.github.com'
   config.gem 'hobo',
-    :version => '0.9.103'
+    :version => '0.9.106'
   
   # Only load the plugins named here, in the order given. By default, all plugins 
   # in vendor/plugins are loaded in alphabetical order.
--- vendor/gems/hobo-0.9.103/init.rb
+++
@@ -1,2 +0,0 @@
-require File.dirname(__FILE__) + "/lib/hobo"
-require 'rails_generator'
