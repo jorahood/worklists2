b67099409e1e750695a565b72a5ca07cf6a1080c
jorahood <jorahood@5cd652fc-3c8e-420e-a729-68eecf2ccb59>
2010-02-15 18:21:51 +0000

    updating composite-primary-keys, making sure rails is vendored (it already was, to 2.3.5)
    git-svn-id: svn+ssh://poblano.uits.indiana.edu/srv/svn/kb-support/trunk/worklists2@21055 5cd652fc-3c8e-420e-a729-68eecf2ccb59

--- config/environment.rb
+++ config/environment.rb
@@ -5,7 +5,7 @@
 # ENV['RAILS_ENV'] ||= 'production'
 
 # Specifies gem version of Rails to use when vendor/rails is not present
-RAILS_GEM_VERSION = '2.3.5' unless defined? RAILS_GEM_VERSION
+#RAILS_GEM_VERSION = '2.3.5' unless defined? RAILS_GEM_VERSION
 
 # Bootstrap the Rails environment, frameworks, and default configuration
 require File.join(File.dirname(__FILE__), 'boot')
@@ -38,7 +38,7 @@ Rails::Initializer.run do |config|
     :lib => 'active_record/connection_adapters/oracle_adapter',
     :source => 'http://gems.rubyonrails.org'
   config.gem 'composite_primary_keys',
-    :version => '2.3.5'
+    :version => '2.3.5.1'
   #rubycas-client 2.0.99.1 makes the rubycas client compatible with IU CAS. I will keep a rebased version available under the iu tag at 
   #http://github.com/jorahood/rubycas-client/
   config.gem 'rubycas-client',
