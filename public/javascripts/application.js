7ce1f5deea9de0336af2b666bd44ab051ab22967
jorahood <jorahood@5cd652fc-3c8e-420e-a729-68eecf2ccb59>
2010-03-23 20:41:02 +0000

    reformat code, encapsulate everything in jQuery(document).ready(function($) so I can use $, add columns object literal and initInputHeaders function from the YUI code
    git-svn-id: svn+ssh://poblano.uits.indiana.edu/srv/svn/kb-support/trunk/worklists2@21477 5cd652fc-3c8e-420e-a729-68eecf2ccb59

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,31 +1,137 @@
-  /* Create an array with the values of all the select options in a column */
-  jQuery.fn.dataTableExt.afnSortData['dom-select'] = function  ( oSettings, iColumn )
-{
-	var aData = [];
-	jQuery( 'td:not(.field-list td):eq('+iColumn+') select', oSettings.oApi._fnGetTrNodes(oSettings) ).each( function () {
-          aData.push(jQuery(this).val());
-	} );
-	return aData;
-}
-// insert dummy links into table headers for clicking in tests
-jQuery(document).ready(function() {
-        jQuery('table.display > thead > tr > th').each(function() {
-          var sAnchor = '<a href="javascript:void(null)">' + jQuery.trim(jQuery(this).text()) + '</a>';
-          jQuery(this).html(sAnchor);
-        })
-// load the dataTables table
-	jQuery('table.display').dataTable( {
-		'aoColumns': [
-			{sType: 'html'}, //docid
-			{sType: 'html'}, //titles
-			null, //approveddate, autodetected
-			null, //modifieddate, autodetected
-			{sType: 'html'}, //domains
-                        null, //owner
-                        null, //visibility
-                        {sType: 'html'},//tags
-			{ sType: 'html' },//notes
-			{ "sSortDataType": "dom-select" }, //workstate
-		]
-	} );
+jQuery(document).ready(function($) {
+  //put some config in my wl2 namespace
+  $.wl2 = {
+    ooColumns : {
+      Docid: {
+        sortable:true
+      },
+      Titles: {
+        sortable:true
+      },
+      Approveddate: {
+        parser: "date",
+        formatter:"date",
+        sortable:true
+      },
+      Modifieddate: {
+        parser: "date",
+        formatter:"date",
+        sortable:true
+      },
+      Birthdate: {
+        parser: "date",
+        formatter:"date",
+        sortable:true
+      },
+      Domains: {
+        sortable:true
+      },
+      Owner: {
+        sortable:true
+      },
+      Author: {
+        sortable:true
+      },
+      Refs: {
+        sortable:true
+      },
+      Refbys: {
+        sortable:true
+      },
+      "Boiler Name": {
+        sortable:true
+      },
+      "Referenced Boilers": {
+        sortable:true
+      },
+      Expirations: {
+        sortable:true
+      },
+      Hotitems: {
+        sortable:true
+      },
+      Importance: {
+        sortable:true
+      },
+      Resources: {
+        sortable:true
+      },
+      Status: {
+        sortable:true
+      },
+      Visibility: {
+        sortable:true
+      },
+      Volatility: {
+        sortable:true
+      },
+      Kbas: {
+        sortable:true
+      },
+      "Kba Bys": {
+        sortable:true
+      },
+      Xtras: {
+        sortable:true
+      },
+      Tags: {
+        sortable:true
+      },
+      Notes: {
+        sortable:true
+      },
+      Workstate: {
+        sortable:true
+      }
+    },
+    sTableSelector : 'table.display'
+  };
+  $.wl2.initInputHeaders = function() {
+    var headers = jQuery(' tr.field-heading-row > th') || [];
+    return headers;
+  };
+  /* For live sorting, create an array with the values of all the select options in a column read from the DOM*/
+  $.fn.dataTableExt.afnSortData['dom-select'] = function  ( oSettings, iColumn )
+  {
+    var aData = [];
+    $( 'td:not(.field-list td):eq('+iColumn+') select', oSettings.oApi._fnGetTrNodes(oSettings) ).each( function () {
+      aData.push($(this).val());
+    } );
+    return aData;
+  }
+  // insert dummy links into table headers for clicking in tests
+  $('table.display > thead > tr > th').each(function() {
+    var sAnchor = '<a href="javascript:void(null)">' + $.trim($(this).text()) + '</a>';
+    $(this).html(sAnchor);
+  })
+  // load the dataTables table
+  $('table.display').dataTable( {
+    'aoColumns': [
+    {
+      sType: 'html'
+    }, //docid
+
+    {
+      sType: 'html'
+    }, //titles
+    null, //approveddate, autodetected
+    null, //modifieddate, autodetected
+    {
+      sType: 'html'
+    }, //domains
+    null, //owner
+    null, //visibility
+    {
+      sType: 'html'
+    },//tags
+
+    {
+      sType: 'html'
+    },//notes
+
+    {
+      "sSortDataType": "dom-select"
+    }, //workstate
+    ]
+  } );
 } );
