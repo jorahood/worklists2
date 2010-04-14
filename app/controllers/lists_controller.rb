67941f77e6eeab6032eccddc8e1adc19850ea393
jorahood <jorahood@5cd652fc-3c8e-420e-a729-68eecf2ccb59>
2010-04-14 14:39:30 +0000

    streamlining additional sorting on doc metadata
    git-svn-id: svn+ssh://poblano.uits.indiana.edu/srv/svn/kb-support/trunk/worklists2@21611 5cd652fc-3c8e-420e-a729-68eecf2ccb59

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -14,16 +14,10 @@ class ListsController < ApplicationController
     @listed_docs =
       @list.listed_docs.apply_scopes(:search => [params[:search], :doc_id, :status, :tag],
       :order_by => parse_sort_param(:workstate),
-      :status_is => params[:status])
-    # check if we need to do special sorting on doc attr. parse_sort_param has
-    # the nice property that it will set @sort_field and @sort_direction only if one of
-    # the parameters passed as an argument was passed as the "sort" url param. Since
-    # the Hobo auto scope :order_by can't handle sorting by the attributes of a belongs_to
-    # association, handle it ourselves by applying our custom named scope
-    # "sort_by_doc_attr" after the vanilla order_by clause checks if "workstate" was
-    # the selected sort param
-    @listed_docs = @listed_docs.sort_by_doc_attr(
-      parse_sort_param('approveddate', 'birthdate', 'modifieddate'))
+      :status_is => params[:status],
+      # check if we need to sort on doc attr. parse_sort_param will set @sort_field and @sort_direction iff the "sort"
+      # url param matches one of the args. 
+      :sort_by_doc_attr => parse_sort_param(:approveddate, :birthdate, :modifieddate))
     @listed_doc_fields = @list.selected_columns.join(', ')
   end
 end
