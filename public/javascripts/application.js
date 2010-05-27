f691cac4f0d3b50673ca7d28a7e353f9e2b30900
jorahood <jorahood@5cd652fc-3c8e-420e-a729-68eecf2ccb59>
2010-05-27 19:45:35 +0000

    using JQuery plugin called Collapsorz to do note collapsing
    git-svn-id: svn+ssh://poblano.uits.indiana.edu/srv/svn/kb-support/trunk/worklists2@21869 5cd652fc-3c8e-420e-a729-68eecf2ccb59

---
+++ public/javascripts/application.js
@@ -0,0 +1,7 @@
+jQuery(document).ready(function($) {
+  $(".collection.notes").collapsorz({
+    showText: "notes",
+    hideText: "Collapse notes",
+    linkLocation: "before"
+  });
+})
---
+++ public/javascripts/jquery.collapsorz.js
@@ -0,0 +1,80 @@
+/* -------------------------------------------------- *
+ * Collapsorz 1.1
+ * Updated: 04/21/09
+ * -------------------------------------------------- *
+ * Author: Aaron Kuzemchak
+ * URL: http://aaronkuzemchak.com/
+ * Copyright: 2008-2009 Aaron Kuzemchak
+ * License: MIT License
+** -------------------------------------------------- */
+
+(function($) {
+	$.fn.collapsorz = function(options) {
+		// default settings
+		var defaults = {
+			toggle: "> *", // elements inside the object to toggle
+			minimum: 1, // number to show in collapsed form
+			showText: "Show", // text for the expand link
+			hideText: "Hide", // text for the collapse link
+			linkLocation: "after", // use "after" or "before" to determine where link displays
+			defaultState: "collapsed", // use "collapsed" or "expanded" to show or hide items by default
+			wrapLink: null // specify HTML code to wrap around the link
+		};
+		var options = $.extend(defaults, options);
+		
+		return this.each(function() {
+			// only execute if there are more than minimum items
+                        var num_togglees = $(options.toggle, this).length;
+			if(num_togglees > options.minimum) {
+				// setup variables
+				var $obj = $(this);
+				var $targets = $(options.toggle, this);
+			
+				// hide the items if necessary
+				if(options.defaultState == "collapsed") {
+					$targets.filter(":lt("+(num_togglees - options.minimum)+")").hide();
+				}
+				
+				// append/prepend the toggle link to the object
+				var $toggler = $('<a href="#" class="toggler"></a>');
+				if(options.linkLocation == "before") {
+					$obj.before($toggler);
+				}
+				else {
+					$obj.after($toggler);
+				}
+				if(options.wrapLink) {
+					$toggler.wrap(options.wrapLink);
+				}
+				
+				// set data, link class, and link text
+				if(options.defaultState == "expanded") {
+					$obj.data("status", "expanded");
+					$toggler.addClass("expanded");
+					$toggler.html(options.hideText);
+				}
+				else {
+					$obj.data("status", "collapsed");
+					$toggler.addClass("collapsed");
+					$toggler.html("Show all " + num_togglees + ' ' + options.showText);
+				}
+				
+				// click actions
+				$toggler.click(function() {
+					if($obj.data("status") == "collapsed") {
+						$targets.filter(":hidden").show();
+						$toggler.html(options.hideText);
+						$obj.data("status", "expanded");
+					}
+					else if($obj.data("status") == "expanded") {
+						$targets.filter(":lt("+(num_togglees - options.minimum)+")").hide();
+						$toggler.html("Show all " + num_togglees + ' ' + options.showText);
+						$obj.data("status", "collapsed");
+					}
+					$(this).toggleClass("collapsed").toggleClass("expanded");
+					return false;
+				});
+			}
+		});
+	}
+})(jQuery);
